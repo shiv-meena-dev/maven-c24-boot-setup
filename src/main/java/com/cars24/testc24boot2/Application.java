e047edba64572327088e657fea592b465c3ac3a9
shiv <shivdayal.meena@cars24.com>
2025-11-10 02:38:24 +0530

    Initial commit: C24 Boot 2.0 test project

---
+++ src/main/java/com/cars24/testc24boot2/Application.java
@@ -0,0 +1,13 @@
+package com.cars24.testc24boot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
